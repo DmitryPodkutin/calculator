390db09d782d5f172f640bcdbf1909a1fb10c289
Dmitry Podkutin <dmitry.podkutin@gmail.com>
2021-02-08 13:13:24 +0300

    refactoring ParserTest

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -3,20 +3,29 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import util.Parser;
 
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
+
 public class ParserTest {
     private final Parser parser = new Parser();
+    private final int firstArabicNumber = 5;
+    private final int secondArabicNumber = 8;
+    private final String firstRomanNumber = "V";
+    private final String secondRomanNumber = "VIII";
+    private final List<String> listOperations = Arrays.asList("+", "-", "*", "/");
 
     @Test
-    public void parse() {
-        Assertions.assertEquals(new MathematicalOperation(5, "+", 8, false), parser.parse("5+8"));
-        Assertions.assertEquals(new MathematicalOperation(5, "-", 8, false), parser.parse("5-8"));
-        Assertions.assertEquals(new MathematicalOperation(5, "/", 8, false), parser.parse("5/8"));
-        Assertions.assertEquals(new MathematicalOperation(5, "*", 8, false), parser.parse("5*8"));
+    public void arabicParse() {
+        listOperations.forEach(operation -> Assertions.assertEquals(new MathematicalOperation(firstArabicNumber, operation, secondArabicNumber, false)
+                , parser.parse(MessageFormat.format("{0}" + operation + "{1}", firstArabicNumber, secondArabicNumber))));
 
-        Assertions.assertEquals(new MathematicalOperation(5, "+", 8, true), parser.parse("V+VIII"));
-        Assertions.assertEquals(new MathematicalOperation(5, "-", 8, true), parser.parse("V-VIII"));
-        Assertions.assertEquals(new MathematicalOperation(5, "/", 8, true), parser.parse("V/VIII"));
-        Assertions.assertEquals(new MathematicalOperation(5, "*", 8, true), parser.parse("V*VIII"));
+    }
+
+    @Test
+    public void romanParse() {
+        listOperations.forEach(operation -> Assertions.assertEquals(new MathematicalOperation(firstArabicNumber, operation, secondArabicNumber, true)
+                , parser.parse(MessageFormat.format("{0}" + operation + "{1}", firstRomanNumber, secondRomanNumber))));
     }
 }
 
